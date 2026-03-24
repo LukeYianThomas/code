6d487f936ac18ad67cd258cb3932f0e51e5596c9
Luke Thomas <lukeyianthomas@outlook.com>
2026-03-24 09:36:48 +0000

    Delete code directory

--- code/side projects/higher or lower.py
+++
@@ -1,6 +0,0 @@
-import random
-answer = random.randint(0,1000)
-while True:
-    if int(input("Guess")) > answer:print("Lower")
-    elif int(input("Guess")) < answer:print("Higher")
-    else:print("You Win!")
